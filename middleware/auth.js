67d6da23e50353d06db8d78767d38aa5960574e2
Lauren <laurendostal@yahoo.com>
2024-07-06 11:59:06 -0400

    added and modified assignment files

---
+++ middleware/auth.js
@@ -0,0 +1,11 @@
+function checkAuth(req, res, next) {
+	// Check req.session.loggedIn to see if the user is logged in
+	// If the user is logged in, call next()
+	// If the user is not logged in, redirect to /login
+	if (req.session && req.session.loggedIn) {
+		return next()
+	}
+	res.redirect('/login')
+}
+
+module.exports = checkAuth
